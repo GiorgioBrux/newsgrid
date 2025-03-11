82dbf1dd1c52fa015f0e685d65ee9175aac1d85c
GiorgioBrux <18328525+GiorgioBrux@users.noreply.github.com>
2025-03-11 17:39:34 +0100

    remove corsproxy

--- src/lib/constants/config.ts
+++ src/lib/constants/config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  API_BASE_URL: 'https://corsproxy.io/?https%3A%2F%2Fkite.kagi.com',
+  API_BASE_URL: 'https://kite.kagi.com',
   SITE_NAME: 'Gio\'s Kite',
   CACHE_DURATION: 5 * 60 * 1000, // 5 minutes
 } as const; 
